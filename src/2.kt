626289f7b49f25084534cc44c99acef625b3f02e
Tictaker3042 <il.lebed2011.gmail.com>
2024-01-19 16:21:19 +0300

    second commit

--- src/2.kt
+++ src/2.kt
@@ -1,19 +1,29 @@
 fun main() {
-    print("Введите 1 число: ")
-    val first = readLine()!!
-    val a: Int = first.toInt()
-    print("Введите 2 число: ")
-    val second = readLine()!!
-    val b: Int = second.toInt()
-    print("Введите операцию над числами: ")
-    val sign = readLine()!!
-    when (sign) {
-        "+" -> {
-            val r = a + b
-            print("" + a + " + " + b)
+    while (true) {
+        print("Введите 1 число: ")
+        val first = readLine()!!
+        val a: Int = first.toInt()
+        print("Введите 2 число: ")
+        val second = readLine()!!
+        val b: Int = second.toInt()
+        print("Введите операцию над числами: ")
+        val sign = readLine()!!
+        when (sign) {
+            "+" -> {
+                val r = a + b
+                println("" + a + " + " + b + " = " + r)
+            }
+            "-" -> println("$a - $b = ${a - b}")
+            "/" -> println(a / b)
+            "*" -> println(a * b)
+            else -> println("Ошибка")
         }
-        "-" -> print("$a - $b = ${a - b}")
-        "/" -> print(a / b)
-        else -> print("Ошибка")
+        println("Введите 1, чтобы продолжить выполнение программы")
+        println("Введите 2, завершить программу")
+        val choose = readLine()!!
+        val ch: Int = choose.toInt()
+        if (ch == 2) {
+            break
+        }
     }
 }
