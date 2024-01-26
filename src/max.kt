d821eb015dd3368e0c5d598da7fec100cfa33c5c
Tictaker3042 <il.lebed2011.gmail.com>
2024-01-26 15:56:32 +0300

    max_without_list

--- src/max.kt
+++ src/max.kt
@@ -8,9 +8,11 @@ fun `fun`() {
 */
 
 fun main() {
-    mylist()
-}
+    /*mylist() */
+    val max_num: Long = Long.MIN_VALUE
+    myfunc2(max_num)
 
+}
 
 fun mylist() {
     val list: MutableList<Int> = mutableListOf()
@@ -39,4 +41,26 @@ fun showfunc(list:MutableList<Int>) {
     println(list)
     val max: Int = list.maxOrNull() ?: 0
     println("Наибольшее число: $max")
+}
+
+
+fun myfunc2(max_num: Long) {
+    var local_max: Long
+    local_max = max_num
+    while (true) {
+        println("Введите число")
+        val type = readLine()!!
+        val num: Long = type.toLong()
+        if (num > local_max) {
+            local_max = num
+        }
+        println("'Enter', чтобы продолжить ввод чисел")
+        println("'q', чтобы завершить ввод чисел")
+        val choose = readLine()!!
+        val ch: String = choose
+        if (ch == "q") {
+            println("Наибольшее число: $local_max")
+            break
+        }
+    }
 }
